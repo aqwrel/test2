9c74eb7a7aecc19e19a7e7af002c473097d840bf
Pasha K <kisilwww@gmail.com>
2022-03-01 15:59:25 +0200

    upd

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,28 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
+  chainWebpack: (config, { isProd, isClient }) => {
+    if (isProd && isClient) {
+      config.optimization.splitChunks({
+        chunks: "initial",
+        maxInitialRequests: Infinity,
+        cacheGroups: {
+          vueVendor: {
+            test: /[\\/]node_modules[\\/](vue|vuex|vue-router)[\\/]/,
+            name: "vue-vendors",
+          },
+          gridsome: {
+            test: /[\\/]node_modules[\\/](gridsome|vue-meta)[\\/]/,
+            name: "gridsome-vendors",
+          },
+          polyfill: {
+            test: /[\\/]node_modules[\\/]core-js[\\/]/,
+            name: "core-js",
+          },
+        },
+      });
+    }
+  },
   siteName: 'Gridsome',
   plugins: []
 }
